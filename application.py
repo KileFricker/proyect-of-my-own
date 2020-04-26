0bfc27b4094e46cc59c83293d816ae871a03ba26
Kile Fricker <lucianovives12@gmail.com>
2020-04-26 13:29:54 -0300

    added class.py application.py templates\firstpage.html

---
+++ application.py
@@ -0,0 +1,7 @@
+from flask import Flask, render_template, jsonify, request
+from class import *
+
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("c:\database")
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+db.init_app(app)
