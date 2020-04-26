b771f6c7c0c4042f5adc8ebdaa0f2db9fc29b33c
Kile Fricker <lucianovives12@gmail.com>
2020-04-26 13:41:42 -0300

    addded login layout logup

--- application.py
+++ application.py
@@ -5,3 +5,7 @@ app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("c:\database")
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db.init_app(app)
+
+@app.route("/")
+def index():
+    
